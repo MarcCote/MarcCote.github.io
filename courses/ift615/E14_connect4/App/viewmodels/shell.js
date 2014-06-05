823692dd8b392cbcaec2d7c0e195ffdef9683cf4
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2014-06-05 20:15:37 +0200

    Changed base url for Connect4 tournament.

--- courses/ift615/E14_connect4/App/viewmodels/shell.js
+++ courses/ift615/E14_connect4/App/viewmodels/shell.js
@@ -6,7 +6,7 @@
             return router.activate('home');
         }
     };
-    window.base_url = ""
+    window.base_url = "./courses/ift615/E14_connect4/"
     window.vm = function() {
     	return shell.router.activeItem();
     }
